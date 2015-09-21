24666f0d2e5f4b5ad79184102f559beb1400e230
jesse keane <me@jessekeane.me>
2015-09-21 14:55:13 -0700

    add a small bin

---
+++ bin/connect.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+
+var path = require('path')
+var fs = require('fs')
+
+var args = require('minimist')
+
+var connect = require('../')
+var pkg = require('../package.json')
+
+module.exports = bin
+
+if (require.main === module) {
+  bin()
+}
+
+function bin () {
+  var options = args(process.argv.slice(2))
+  var streamOptions = {}
+  var stream
+
+  if (options.help) {
+    return help()
+  }
+
+  if (options.version) {
+    return version()
+  }
+
+  if (!options.username) {
+    if (options._.length) {
+      options.username = options._[0]
+    } else {
+      return help()
+    }
+  }
+
+  if (!options.password) {
+    if (options._.length > 1) {
+      options.password = options._[1]
+    } else {
+      return help()
+    }
+  }
+
+  if (options.uri) {
+    streamOptions.uri = options.uri
+  }
+
+  stream = connect(options.username, options.password, streamOptions)
+
+  stream.on('data', console.log.bind(console))
+
+  if (options.offset) {
+    stream.write({resume_offset: options.offset})
+  } else if (options.earliest) {
+    stream.write({start: 'EARLIEST'})
+  } else {
+    stream.write({start: 'LATEST'})
+  }
+}
+
+function help () {
+  version()
+
+  fs.createReadStream(path.resolve(__dirname, '..', 'help.txt'))
+    .pipe(process.stderr)
+}
+
+function version () {
+  console.error('Connect client version ' + pkg.version)
+}
